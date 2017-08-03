a740e0682511541fc0dc490e538488665933e3bb
System <none>
2017-08-03 04:59:05 +0000

    🚉🎿 Updated with Glitch

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
-/*
- * Copyright (c) 2016 ObjectLabs Corporation
- * Distributed under the MIT license - http://opensource.org/licenses/MIT
- *
- * Written with: mongodb@2.1.3
- * Documentation: http://mongodb.github.io/node-mongodb-native/
- * A Node script connecting to a MongoDB database given a MongoDB Connection URI.
-*/
 
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var mongodb = require('mongodb');
 var validUrl = require('valid-url');
 var app = express();
-
+var shortid = require('shortid');//for generating unique ids for urls
 
 app.use(express.static('public'));
 
@@ -28,25 +17,14 @@ app.get("/", function (request, response) {
 //check if url exists? 
 //create new 8000 check if it exists loop?
 
-
-// app.get('/', function(req, res) {
-  
-// })
-//want to return this 
-// {
-// original_url: "https://www.facebook.com",
-// short_url: "https://little-url.herokuapp.com/7963"
-// }
-
-
 // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname, details set in .env
 var uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
 
 var id = 0;
 
 function randomURL(){
-  //check if the new random number is already in the database
-  id = Math.floor(Math.random() * (10000 - 1000) + 1000);
+  // id = Math.floor(Math.random() * (10000 - 1000) + 1000);
+  id = shortid.generate();
   return "https://way-shorter-url.glitch.me/" + id;
 }
 
@@ -69,28 +47,26 @@ app.get('/new/*', function(req, res){
           original_url: oldURL, urlID: id
         }];
         
-      mongodb.MongoClient.connect(uri, function(err, db) {
-        if(err) throw err;
-        //
-        var urls = db.collection('urls');
-        urls.insert(databaseEntry, function(err, result) {
+        mongodb.MongoClient.connect(uri, function(err, db) {
           if(err) throw err;
-        console.log('ive written to the db')
-        })
-
+          var urls = db.collection('urls');
+                urls.insert(databaseEntry, function(err, result) {
+                  if(err) throw err;
+                console.log('ive written to the db');
+            });    
+            });
+        
+        res.end(JSON.stringify(urlObject));//display the two urls
+    } else {//it's NOT valid 
+        console.log("its not valid url");
+        urlObject.error = "Wrong url format, make sure you have a valid protocol and real site.";
+        res.end(JSON.stringify(urlObject));
+        }
 });
-      res.end(JSON.stringify(urlObject));//display the two urls
-} else {//it's NOT valid 
-    console.log("its not valid url");
-    urlObject.error = "Wrong url format, make sure you have a valid protocol and real site.";
-    res.end(JSON.stringify(urlObject));
-    }
+// app.get(/\/([1-9]{1}\d{3})$/, function (req, res) {//regex looking for /1000 through /9999
+app.get('/*', function (req, res) {
   
-
-});
-app.get(/\/([1-9]{1}\d{3})$/, function (req, res) {//regex looking for /1000 through /9999
-  
-  var searchedID = parseInt(req.params[0]);
+  var searchedID = req.params[0];
   console.log("I'm in the redirect app.get - here's the searched id " + searchedID + "its type is " + typeof searchedID);
   // res.send(JSON.stringify(searchedID));
   var lookedUpUrl = {};
@@ -125,87 +101,8 @@ app.get(/\/([1-9]{1}\d{3})$/, function (req, res) {//regex looking for /1000 thr
   // res.end(JSON.stringify(lookedUpUrl));
 });
 
-// app.get("/", function (request, response) {
-//   response.send(dbSongs);
-// });
-
 // listen for requests :)
 var listener = app.listen("3000", function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-  // var songs = db.collection('songs');
-  // dbSongs+="Creating collection 'songs'<br />";
-  
-   // Note that the insert method can take either an array or a dict.
-
-//   songs.insert(seedData, function(err, result) {
-    
-//     if(err) throw err;
-
-//     /*
-//      * Then we need to give Boyz II Men credit for their contribution
-//      * to the hit "One Sweet Day".
-//      */
-
-//     songs.update(
-//       { song: 'One Sweet Day' }, 
-//       { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-//       function (err, result) {
-        
-//         if(err) throw err;
-
-//         /*
-//          * Finally we run a query which returns all the hits that spend 10 or
-//          * more weeks at number 1.
-//          */
-
-//         songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-//           if(err) throw err;
-
-//           docs.forEach(function (doc) {
-//             console.log(
-//               'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
-//               ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-//             );
-//             dbSongs+="Adding "+doc['artist']+" - "+doc['song']+" into 'songs'<br />";
-//           });
-         
-//           // Since this is an example, we'll clean up after ourselves.
-//           songs.drop(function (err) {
-//             dbSongs+="Dropping collection 'songs'<br />";
-//             if(err) throw err;
-          
-            // Only close the connection when your app is terminating.
-            // db.close(function (err) {
-            //   dbSongs+="Closing db " + process.env.DB;
-            //   dbSongs+="<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div>";
-            //   if(err) throw err;
-            // });
-        //   });
-        // });
-  //     }
-  //   );
-  // });
-// Create seed data
-// var seedData = [
-//   {
-//     decade: '1970s',
-//     artist: 'Debby Boone',
-//     song: 'You Light Up My Life',
-//     weeksAtOne: 10
-//   },
-//   {
-//     decade: '1980s',
-//     artist: 'Olivia Newton-John',
-//     song: 'Physical',
-//     weeksAtOne: 10
-//   },
-//   {
-//     decade: '1990s',
-//     artist: 'Mariah Carey',
-//     song: 'One Sweet Day',
-//     weeksAtOne: 16
-//   }
-// ];
